f3f117ec613eb59d80d24d93e03e1ef8638235ac
metagoto <runpac314@gmail.com>
2010-03-19 01:36:21 +0100

    first matos

---
+++ include/r8t/parser/standard_unicode.hpp
@@ -0,0 +1,207 @@
+#pragma once
+
+#include <string>
+
+#define BOOST_SPIRIT_UNICODE
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_container.hpp>
+#include <boost/spirit/include/phoenix_statement.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/regex/pending/unicode_iterator.hpp>
+
+#include <boost/algorithm/string/replace.hpp>
+
+
+namespace r8t { namespace parser
+{
+
+    using boost::spirit::unicode::char_;
+    using boost::spirit::unicode::space;
+    using boost::spirit::qi::grammar;
+    using boost::spirit::qi::rule;
+    using boost::spirit::qi::_val;
+    using boost::spirit::qi::_r1;
+    using boost::spirit::qi::_1;
+    using boost::spirit::qi::lit;
+    using boost::spirit::qi::eps;
+    using boost::spirit::qi::eol;
+    using boost::phoenix::function;
+
+    typedef boost::spirit::char_encoding::unicode unicode;
+    typedef boost::uint32_t uchar; // a unicode code point
+
+
+    namespace detail
+    {
+        struct push_utf8
+        {
+            template <typename S, typename C>
+            struct result { typedef void type; };
+
+            void operator()(std::string& buf, uchar code_point) const
+            {
+                typedef std::back_insert_iterator<std::string> insert_iter;
+                insert_iter out_iter(buf);
+                boost::utf8_output_iterator<insert_iter> utf8_iter(out_iter);
+                *utf8_iter++ = code_point;
+            }
+        };
+
+        struct push_plain
+        {
+            template <typename S, typename C>
+            struct result { typedef void type; };
+
+            void operator()(std::string& buf, std::string const& plain) const
+            {
+                std::string str(boost::algorithm::replace_all_copy(plain, "\n", "\\n"));
+                boost::algorithm::replace_all(str, "'", "\\'");
+                buf.append("__pr('").append(str).append("');");
+            }
+        };
+
+        struct push_expr
+        {
+            template <typename S, typename C>
+            struct result { typedef void type; };
+
+            void operator()(std::string& buf, std::string const& expr) const
+            {
+                buf.append("__p(").append(expr).append(");");
+            }
+        };
+
+        struct push_block
+        {
+            template <typename S, typename C>
+            struct result { typedef void type; };
+
+            void operator()(std::string& buf, std::string const& block) const
+            {
+                if (block.size()) {
+                    buf += block;
+                    std::string::iterator it = buf.end()-1;
+                    if (*it == ':')
+                        *it = '{';
+                    else
+                        buf += ";";
+                }
+            }
+        };
+
+
+    } // ns detail
+
+
+
+
+template <typename Iterator>
+struct standard_unicode : grammar<Iterator, unicode, std::string()>
+{
+    standard_unicode() : standard_unicode::base_type(start)
+    {
+
+        function<detail::push_utf8>   push_utf8;
+        function<detail::push_plain>  push_plain;
+        function<detail::push_expr>   push_expr;
+        function<detail::push_block> push_block;
+
+
+        open_block   = lit("{%") >> *space;
+        open_expr    = lit("{{") >> *space;
+        open_comment = lit("{#") >> *space;
+
+        close_block  = *space >> lit("%}") >> -eol; // eat immediate end of line
+        close_expr   = *space >> lit("}}");
+        close_comment= *space >> lit("#}") >> -eol;
+
+
+        start =
+               *( directive                [_val += _1]
+                | comment
+                | plain                    [push_plain(_val, _1)]
+                )
+        ;
+
+
+        open_marker =
+                  open_block
+                | open_expr
+                | open_comment
+        ;
+
+
+        comment =
+                  open_comment
+               >> *(char_ - close_comment)
+               >> close_comment
+        ;
+
+
+        plain =
+                +(char_ - open_marker)     [push_utf8(_val, _1)]
+        ;
+
+
+        directive =
+                  block                    [push_block(_val, _1)]
+                | expr                     [push_expr(_val, _1)]
+        ;
+
+
+        block =
+                  open_block
+               >> ( block_end(_val) | block_begin )
+               >> close_block
+        ;
+
+
+        block_begin =
+                +(char_ - close_block)     [push_utf8(_val, _1)]
+        ;
+
+
+        block_end =
+                  lit("end")
+               >> *(char_ - close_block)
+               >> eps[_r1 += '}']
+        ;
+
+
+        expr =
+                  open_expr
+               >> +(char_ - close_expr)    [push_utf8(_val, _1)]
+               >> close_expr
+        ;
+
+
+    }
+
+    rule<Iterator, unicode, std::string()> start
+                                         , directive
+                                         , expr
+                                         , plain;
+
+    rule<Iterator, unicode> open_block
+                          , open_expr
+                          , open_comment
+                          , open_marker;
+
+    rule<Iterator, unicode> close_block
+                          , close_expr
+                          , close_comment
+                          , comment;
+
+    rule<Iterator, unicode, std::string()> block
+                                         , block_begin;
+
+    rule<Iterator, unicode, void(std::string&)> block_end;
+
+
+};
+
+} } // ns
+
+
---
+++ test.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <boost/foreach.hpp>
+#include <boost/algorithm/string/replace.hpp>
+
+#include "include/r8t/parser/standard_unicode.hpp"
+
+
+
+bool test_file(char const* filename, std::string& input, std::string& expected)
+{
+    input.clear();
+    expected.clear();
+
+    std::string filename_exp = std::string(filename) + ".exp";
+
+    std::ifstream in(filename, std::ios_base::in);
+    if (!in) {
+        std::cout << "error: could not open file: " << filename << std::endl;
+        return false;
+    }
+
+    std::ifstream in_exp(filename_exp.c_str(), std::ios_base::in);
+    if (!in_exp) {
+        std::cout << "error: could not open file: " << filename_exp << std::endl;
+        return false;
+    }
+
+    in.unsetf(std::ios::skipws); // keep white space
+    std::copy(
+        std::istream_iterator<char>(in),
+        std::istream_iterator<char>(),
+        std::back_inserter(input));
+
+    in_exp.unsetf(std::ios::skipws);
+    std::copy(
+        std::istream_iterator<char>(in_exp),
+        std::istream_iterator<char>(),
+        std::back_inserter(expected));
+    return true;
+}
+
+
+bool test(std::string const& input, std::string const& expected, char const* filename = 0)
+{
+    using boost::algorithm::replace_all_copy;
+
+    typedef boost::u8_to_u32_iterator<std::string::const_iterator> iterator_type;
+
+    iterator_type first = input.begin();
+    iterator_type last = input.end();
+
+    r8t::parser::standard_unicode<iterator_type> p;
+
+    std::string result;
+
+    if (boost::spirit::qi::parse(first, last, p, result) && first == last && result == expected) {
+        std::cout << " PASS "
+                  << (filename ? filename : replace_all_copy(input, "\n", "\\n"));
+    }
+    else {
+        std::cout << "-FAIL ";
+        if (filename) {
+            std::string filename_out = std::string(filename) + ".out";
+            std::cout << filename << " -> " << filename_out;
+            std::ofstream out(filename_out.c_str(), std::ios_base::out);
+            if (!out) {
+                std::cout << "error: could not write to file: " << filename_out << std::endl;
+            }
+            else {
+                out << result << "\n";
+            }
+        }
+        else {
+            std::cout << replace_all_copy(input, "\n", "\\n") << "\n"
+                      << "  exp " << replace_all_copy(expected, "\n", "\\n") << "\n"
+                      << "  out " << replace_all_copy(result, "\n", "\\n") << "\n"
+                      << "  siz " << result.size() << " " << expected.size();
+        }
+    }
+    std::cout << std::endl;
+}
+
+
+int main()
+{
+
+    test(""
+        ,""
+    );
+    test("a"
+        ,"__pr('a');"
+    );
+    test("ab\nc"
+        ,"__pr('ab\\nc');"
+    );
+    test("{% azf = 42 %}"
+        ,"azf = 42;"
+    );
+    test("{# azf #}"
+        ,""
+    );
+    test("abc{%for(i in a):%}{%end%}"
+        ,"__pr('abc');for(i in a){};"
+    );
+    test("abc{%for(i in a):%}\nraw{#com#}war{{sa}}{%end%}"
+        ,"__pr('abc');for(i in a){__pr('raw');__pr('war');__p(sa);};"
+    );
+
+    std::string input;
+    std::string expected;
+
+    const char* files[] = {
+        "tests/parser/basic_1.txt"
+       ,"tests/parser/basic_2.txt"
+    };
+
+    BOOST_FOREACH( const char* f, files )
+    {
+        if (test_file(f, input, expected))
+            test(input, expected, f);
+    }
+
+
+
+
+};
+
